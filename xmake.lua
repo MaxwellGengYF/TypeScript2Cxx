1773a36c41578d23a73d14a80e07b9913721ff41
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2024-05-21 11:29:11 +0800

    build

---
+++ xmake.lua
@@ -0,0 +1,16 @@
+set_xmakever('2.9.1')
+add_rules('mode.release', 'mode.debug', 'mode.releasedbg')
+set_policy('build.ccache', not is_plat('windows'))
+set_toolchains('llvm')
+
+includes('test/xmake/xmake_func.lua', 'test/EASTL/xmake.lua')
+
+target('test')
+_config_project({
+    project_kind = 'binary',
+    enable_exception = true
+})
+set_pcxxheader('test/core.h')
+add_files('test/test.cpp')
+add_deps('eastl')
+add_includedirs('.')
